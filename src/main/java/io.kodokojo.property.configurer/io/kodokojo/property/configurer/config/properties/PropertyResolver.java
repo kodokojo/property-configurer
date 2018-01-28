292198aa7649a0951b8aace22de0e2dd6dd8f758
jpthiery <jpthiery@xebia.fr>
2018-01-28 17:16:17 +0100

    Java 9 compliante
    Improve property resolving.
    Allow user to valid is Property configuration

--- src/main/java/io.kodokojo.property.configurer/io/kodokojo/property/configurer/config/properties/PropertyResolver.java
+++ src/main/java/io.kodokojo.property.configurer/io/kodokojo/property/configurer/config/properties/PropertyResolver.java
@@ -1,54 +1,36 @@
 /**
  * Kodo Kojo - Software factory done right
- * Copyright © 2017 Kodo Kojo (infos@kodokojo.io)
- * <p>
+ * Copyright © 2018 Kodo Kojo (infos@kodokojo.io)
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * <p>
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * <p>
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package io.kodokojo.property.configurer.config.properties;
 
-/*
- * #%L
- * commons-commons
- * %%
- * Copyright (C) 2016 Kodo-kojo
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/gpl-3.0.html>.
- * #L%
- */
 
 import io.kodokojo.property.configurer.config.properties.provider.PropertyValueProvider;
 
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.math.BigDecimal;
 
-import static org.apache.commons.lang.StringUtils.isNotBlank;
 
 public class PropertyResolver {
+
+    public static final String VALID_METHOD_NAME = "valid";
 
     private final PropertyValueProvider propertyValueProvider;
 
@@ -62,22 +44,50 @@ public class PropertyResolver {
         this.internalInvoker = new InternalInvoker();
     }
 
+    @SuppressWarnings("unchecked")
     public <T extends PropertyConfig> T createProxy(Class<T> propertyConfig) {
         return (T) Proxy.newProxyInstance(PropertyResolver.class.getClassLoader(), new Class[]{propertyConfig}, internalInvoker);
     }
 
     private class InternalInvoker implements InvocationHandler {
+
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            Key keyAnnotation = method.getAnnotation(Key.class);
-            if (keyAnnotation == null) {
-                return method.invoke(proxy, args);
+            Class<?> returnType = method.getReturnType();
+            if (VALID_METHOD_NAME.equals(method.getName())) {
+                //  Java 9 rules :)
+                //  Fix found here :
+                //  https://github.com/brharrington/spectator/commit/b43f0013ebbfdb5c142210b54afbfb2e31cc07b5
+
+                //  Invoke the default method implementation with java 9 reflect
+                final Class<?> declaringClass = method.getDeclaringClass();
+                MethodType rt = MethodType.methodType(returnType);
+                return MethodHandles.lookup()
+                        .findSpecial(declaringClass, method.getName(), rt, declaringClass)
+                        .bindTo(proxy)
+                        .invokeWithArguments(args);
+            } else {
+                Key keyAnnotation = method.getAnnotation(Key.class);
+                if (keyAnnotation == null) {
+                    return method.invoke(proxy, args);
+                }
+                Object value = propertyValueProvider.providePropertyValue(returnType, keyAnnotation.value());
+                if (value == null) {
+                    try {
+                        value = getDefaultValue(returnType, keyAnnotation.defaultValue());
+                    } catch (NumberFormatException e) {
+                        if (Number.class.isAssignableFrom(returnType)) {
+                            throw new IllegalStateException("Invalid value for key '" + keyAnnotation.value() + "' : '" + keyAnnotation.defaultValue() + "'", e);
+                        } else {
+                            throw e;
+                        }
+                    }
+                }
+                if (value == null) {
+                    throw new IllegalStateException("Unable to provide property value for key '" + keyAnnotation.value() + "'.");
+                }
+                return value;
             }
-            Object value = propertyValueProvider.providePropertyValue(method.getReturnType(), keyAnnotation.value());
-            if (isNotBlank(keyAnnotation.defaultValue()) && value == null) {
-                value = getDefaultValue(method.getReturnType(), keyAnnotation.defaultValue());
-            }
-            return value;
         }
     }
 
---
+++ src/test/java/io.kodokojo.property.configurer/io/kodokojo/property/configurer/config/properties/PropertyResolverTest.java
@@ -0,0 +1,106 @@
+/**
+ * Kodo Kojo - Software factory done right
+ * Copyright © 2018 Kodo Kojo (infos@kodokojo.io)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.kodokojo.property.configurer.config.properties;
+
+import io.kodokojo.property.configurer.config.properties.provider.PropertyValueProvider;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+class PropertyResolverTest {
+
+    public interface ConfigSimple extends PropertyConfig {
+        @Key("props.a")
+        String a();
+    }
+
+    public interface ValidateConfigSimple extends PropertyConfig {
+        @Key("props.a")
+        String a();
+
+        @Override
+        default void valid() {
+            throw new IllegalStateException("fake exception");
+        }
+    }
+
+    public interface DefaultNumber extends PropertyConfig {
+
+        @Key(value = "props.number", defaultValue = "")
+        Long phoneNumber();
+
+    }
+
+    @Test
+    public void default_validated_config() {
+        ConfigSimple configSimple = createConfig(ConfigSimple.class, createPropertyValueProvider(), true);
+        assertThat(configSimple).isNotNull();
+        assertThat(configSimple.a()).isEqualTo("a");
+    }
+
+    @Test
+    public void override_validated_config() {
+        try {
+            createConfig(ValidateConfigSimple.class, createPropertyValueProvider(), true);
+            fail("Expected an exception");
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage()).isEqualTo("fake exception");
+        }
+    }
+
+    @Test
+    public void emptyDefaultValueThrowException() {
+
+        DefaultNumber defaultNumber = createConfig(DefaultNumber.class, createPropertyValueProvider(), false);
+        boolean iseSent = false;
+        try {
+            defaultNumber.phoneNumber();
+            fail("Expecting an exception.");
+        } catch (NumberFormatException n) {
+            fail("Not expected exception", n);
+        } catch (IllegalStateException e) {
+            iseSent = true;
+            assertThat(e.getMessage()).contains("Invalid value for key 'props.number' : ''");
+        }
+        assertThat(iseSent).isTrue();
+    }
+
+    private static PropertyValueProvider createPropertyValueProvider() {
+        return new PropertyValueProvider() {
+            @SuppressWarnings("unchecked")
+            @Override
+            public <T> T providePropertyValue(Class<T> classType, String key) {
+                switch (key) {
+                    case "props.a":
+                        return (T) "a";
+                    case "props.b":
+                        return (T) "b";
+                }
+                return null;
+            }
+        };
+    }
+
+    private <T extends PropertyConfig> T createConfig(Class<T> configClass, PropertyValueProvider valueProvider, boolean validated) {
+        PropertyResolver resolver = new PropertyResolver(valueProvider);
+        T proxy = resolver.createProxy(configClass);
+        if (validated) proxy.valid();
+        return proxy;
+    }
+}
